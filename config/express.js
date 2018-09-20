e991e728770b95e73f57e3a5528950e90b86684e
mshadeveloper <mshadeveloper@gmail.com>
2018-09-21 01:21:58 +0300

    fix enviroments

--- config/express.js
+++ config/express.js
@@ -1,7 +1,23 @@
 var express=require('express');
+var morgan=require('morgan');
+var bodyparser=require('compression');
+var bodyparser=require('body-parser');
+var methodoverride=require('method-override')
 module.exports=function(){
 
   var app=express()
+
+  if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+  }
+
+  else if(process.env.NODE_ENV==='production'){
+    app.use(compress())
+  }
+
+  app.use(bodyparser.json())
+  app.use(methodoverride())
+
   require('../app/routing/index.server.routes.js')(app);
   return app;
 }
